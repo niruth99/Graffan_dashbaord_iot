3d8de73a1df94bbea976e2c7ba2feb1d5b17903b
coldtaco <36543008+coldtaco@users.noreply.github.com>
2024-03-11 16:25:33 +1100

    Delete amd64 bionic directory

--- amd64 bionic/install.sh
+++
@@ -1,5 +0,0 @@
-sudo dpkg -i ./containerd.io_1.6.9-1_amd64.deb \
-  ./docker-ce_24.0.2-1~ubuntu.18.04~bionic_amd64.deb \
-  ./docker-ce-cli_24.0.2-1~ubuntu.18.04~bionic_amd64.deb \
-  ./docker-buildx-plugin_0.10.5-1~ubuntu.18.04~bionic_amd64.deb \
-  ./docker-compose-plugin_2.6.0~ubuntu-bionic_amd64.deb
