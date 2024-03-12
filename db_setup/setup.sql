4e0dbdbeddbb65f73f64c1c811dc6d3d559dd501
coldtaco <36543008+coldtaco@users.noreply.github.com>
2024-03-12 16:55:18 +1100

    add data links

--- db_setup/setup.sql
+++ db_setup/setup.sql
@@ -4,8 +4,12 @@ CREATE TABLE public.data (
 --    used_by bigint,
     detected_on timestamp without time zone,
 --    used_on timestamp without time zone,
-    device character varying(256)
+    device character varying(256),
+    probe_id bigint
 );
+
+create unique index data_idx on public.data(probe_id);
+create index detected_on_idx on public.data(detected_on);
 
 CREATE TABLE public.timeseries (    
     detected_on timestamp without time zone,
--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -12,8 +12,17 @@ max_count_per_device = 7
 
 def main():
     sql = SQLInterface()
+    data_max = sql.execute_pd('select max(probe_id) from data;')
+    print(data_max.shape, len(data_max))
+    print(data_max)
+    if data_max['max'][0] is None:
+        ind = 0
+    else:
+        ind = data_max['max'][0] + 1
+
     while True:
         counts = [random.randint(0, max_count_per_device) for x in range(n_devices)]
+        inds = list(range(ind, ind + sum(counts)))
 
         df_dev_name = []
 
@@ -23,11 +32,13 @@ def main():
         # times = [time.time() - random.random() for x in range(sum(counts))]
         times = [time.time() for x in range(sum(counts))]
 
-        df = pd.DataFrame({'device':df_dev_name, 'detected_on':times})
+        df = pd.DataFrame({'device':df_dev_name, 'detected_on':times, 'probe_id': inds})
         df['detected_on'] = pd.to_datetime(df['detected_on'], unit='s')
         # print(df.to_dict('split'))
         
         sql.insert_pd(df, 'data')
+
+        ind += sum(counts)
 
         time.sleep(1)
 
@@ -48,4 +59,4 @@ def summary():
         time.sleep(1)
 
 if __name__ == '__main__':
-    summary()
+    main()
