2ef0be574485fa376e6956357cb571d0ec283b60
coldtaco <36543008+coldtaco@users.noreply.github.com>
2024-03-26 09:06:48 +1100

    fixed live data

--- scripts/live_data.py
+++ scripts/live_data.py
@@ -6,15 +6,15 @@ import os
 import argparse
 import time
 from generate_data import _init, predict_signatures
-from multiprocessing import Pipe, Pool, Process, connection
+from multiprocessing import Pipe, Pool, Process, Queue, connection
 from iotsignature import Signature
 
 def ignore(*args, **kwargs):
     pass
 
-print = ignore
+# print = ignore
 
-def work(pipe:connection.Connection):
+def work(unread_queue:Queue, read_queue:Queue):
     i = _init()
     sql = i['sql']
     verification_base = i['verification_base']
@@ -23,15 +23,18 @@ def work(pipe:connection.Connection):
 
     while True:
         try:
-            signatures = pipe.recv()
+            unread:list[str] = unread_queue.get()
+            for u in unread:
+                signatures = read_pcap(u)
             
-            predict_signatures(
-                sql,
-                verification_base,
-                signatures, 
-                sorter,
-                features,
-            )
+                predict_signatures(
+                    sql,
+                    verification_base,
+                    signatures, 
+                    sorter,
+                    features,
+                )
+                read_queue.put(u)
         except:
             traceback.print_exc()
         # sys.stdout.flush()
@@ -55,61 +58,49 @@ def read_pcap(folder:str) -> list[Signature]:
 
 def main(folder:str, interval:float, threads:float):
     last_read = time.time()
-    write_pipes, read_pipes = [], []
+    
+    unread_queue = Queue()
+    read_queue = Queue()
 
-    print("Init pipes")
-    for x in range(threads):
-        r, w = Pipe()
-        read_pipes.append(r)
-        write_pipes.append(w)
-    print('Pipes inited')
-
-    processes = [Process(target = work, args = [p]) for p in read_pipes]
-    print('Created all processes')
+    processes = [Process(target = work, args = [unread_queue, read_queue]) for p in range(threads)]
     [p.start() for p in processes]
-    print('All processes started')
     # with Pool(threads) as pool:
     #     pool.map(work, read_pipes)
-    print('Finished loading')
     read_files = set()
     while True:
-        print(f'start while {time.time() - last_read:.4f}')
 
-        unread = [os.path.join(folder, f) for f in os.listdir(folder)]
-        x = []
-
-        print(f'iterate files {time.time() - last_read:.4f}')
-
-        for u in unread:
-            for f in os.listdir(u):
-                print(f)
-                if f.endswith('pcap'):
-                    x.append(os.path.join(u, f))
-        print(f'end_iterate {time.time() - last_read:.4f}')
-
-
-        res = []
-
-        for f in x:
-            if f in read_files: continue
-            read_files.add(f)
-            print(f)
-
-            r = read_pcap(f)
-            print(f'{len(r)=} {time.time() - last_read:.4f}')
-            
-            res.extend(r)
-            os.remove(f)
-        print(f'end read pcap {time.time() - last_read:.4f}')
+        unread = []
         
-        # Split into threads
-        for x in range(threads):
-            wp = write_pipes[x]
-            wp.send(res[x::threads])
-        print(f'send pipes {time.time() - last_read:.4f}')
+        # Iterate through folder to find pcaps
+        for sub_f in os.listdir(folder):
+            sub_f = os.path.join(folder, sub_f)
+            # for sub_sub_f in os.listdir(sub_f):
+            #     sub_sub_f = os.path.join(sub_f, sub_sub_f)
+            for file in os.listdir(sub_f):
+                file = os.path.join(sub_f, file)
+                if file.endswith('pcap') and (file not in read_files):
+                    unread.append(file)
+                    read_files.add(file)
+        
+        # Evenly spread load to threads
+        # for x in range(threads):
+        #     unread_queue.put(unread[x::threads])
+
+        # One pcap file per loop
+        for u in unread:
+            print(u)
+            unread_queue.put([u])
+        
+        while not read_queue.empty():
+            r = read_queue.get_nowait()
+            read_files.add(r)
+            os.remove(r)
+
+        for ix, p in enumerate(processes):
+            if not p.is_alive():
+                processes[ix] = Process(target = work, args = [unread_queue, read_queue])
         
         last_read = time.time()
-        print(interval-(time.time() - last_read), time.time()-last_read)
         sys.stdout.flush()
         time.sleep(interval-(time.time() - last_read))
     
@@ -155,8 +146,8 @@ def main(folder:str, interval:float, threads:float):
 if __name__ == '__main__':
     argument_parser = argparse.ArgumentParser(description="Read live data from specific folder")
     argument_parser.add_argument('-s', '--source', type=str, required=True ,help="Source folder")
-    argument_parser.add_argument('-i', '--interval',type=float, required=False ,help="How often to read source folder", default = 1)
-    argument_parser.add_argument('-t', '--threads',type=float, required=False ,help="Number of threads", default = 10)
+    argument_parser.add_argument('-i', '--interval',type=float, required=False ,help="How often to read source folder", default = 0.1)
+    argument_parser.add_argument('-t', '--threads',type=float, required=False ,help="Number of threads", default = os.cpu_count() - 1)
 
     args = argument_parser.parse_args()
     main(args.source, args.interval, args.threads)
