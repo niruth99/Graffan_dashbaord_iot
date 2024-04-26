549f9010fb8ef8e8dcf691e4ff094ccf9ad4176f
coldtaco <36543008+coldtaco@users.noreply.github.com>
2024-04-26 14:53:14 +1000

    Update pcap_copy.py

--- pcap_copy.py
+++ pcap_copy.py
@@ -3,8 +3,11 @@ import time
 import os
 import random
 
+# Randomly selects a pcap file to copy to dest
+
 # Specify the folder path
 folder_path = '/path/to/folder'
+dest_path = '/path/to/dest'
 
 # List all files in the folder
 
@@ -17,6 +20,7 @@ while True:
     f = random.randint(len(files))
     f = files[f]
     source_file = os.path.join(folder_path, f)
-    shutil.copy(source_file, f'{i}.pcap')
+    dest_name = os.path.join(dest_path, f'{i}.pcap')
+    shutil.copy(source_file, dest_name)
     i += 1
     time.sleep(3)
